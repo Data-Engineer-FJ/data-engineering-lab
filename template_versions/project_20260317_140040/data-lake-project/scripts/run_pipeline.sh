726a02cfa66b1dbcc49baeff3210f1ff8189c941
Fredy Johel Peña Álvarez <186350549+Data-Engineer-FJ@users.noreply.github.com>
2026-03-18 22:32:07 -0500

    Delete template_versions/project_20260317_140040 directory

--- template_versions/project_20260317_140040/data-lake-project/scripts/run_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Ejecutando pipelines medallion"
-
-python src/pipelines/bronze/ingest_personas.py
-python src/pipelines/silver/transform_personas.py
-python src/pipelines/gold/aggregate_personas.py
